f4663d99a2f236554c82f17de83c7c3bdac108aa
Rémi Weng <ixartz@users.noreply.github.com>
2014-05-13 01:47:07 +0200

    Add add_dir cmake function that finds all .h and .cpp in a directory

---
+++ src/base/menu.cpp
@@ -0,0 +1,9 @@
+//
+//  menu.cpp
+//  multi-agent-system
+//
+//  Created by Ixi on 12/05/14.
+//
+//
+
+#include "menu.h"
---
+++ src/base/menu.h
@@ -0,0 +1,14 @@
+//
+//  menu.h
+//  multi-agent-system
+//
+//  Created by Ixi on 12/05/14.
+//
+//
+
+#ifndef __multi_agent_system__menu__
+#define __multi_agent_system__menu__
+
+#include <iostream>
+
+#endif /* defined(__multi_agent_system__menu__) */
